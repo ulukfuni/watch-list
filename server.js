830dada6510bf5c8baf81add04b37bf91faae399
Viet <vtn1989@gmail.com>
2016-03-22 21:31:31 -0400

    able to xray player name and stats in one big array

--- server.js
+++ server.js
@@ -6,8 +6,10 @@ app.use(express.static(__dirname + '/'));
 app.listen(process.env.PORT || 8000);
 console.log('Magic happens on port 8000');
 
-xray('http://www.rotoworld.com/log/nba/1967/anthony-davis', '.statstable')(function(err, obj){
+xray('http://www.rotoworld.com/log/nba/1967/anthony-davis', {
+	playerName: 'h1',
+	stats: xray('.statstable', ['.statstable:not(#cp1_ctl02_tblDepthCharts) tr td'])})(function(err, obj){
   console.log(obj);
-});
+}).write('results.json');
 
 exports = module.exports = app;
