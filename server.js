d38d7e205bab904558594a438986ac064a8e3f2e
Viet <vtn1989@gmail.com>
2016-03-22 21:49:39 -0400

    comment about solution for big array reduce

--- server.js
+++ server.js
@@ -9,7 +9,11 @@ console.log('Magic happens on port 8000');
 xray('http://www.rotoworld.com/log/nba/1967/anthony-davis', {
 	playerName: 'h1',
 	stats: xray('.statstable', ['.statstable:not(#cp1_ctl02_tblDepthCharts) tr td'])})(function(err, obj){
-  console.log(obj);
-}).write('results.json');
+
+  console.log(obj.stats);
+});
+//big array reduce
+// array.map(function(val, index, arr){})
+// inside: push to inner array while index +1 is != 0 for i%22
 
 exports = module.exports = app;
