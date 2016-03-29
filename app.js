1c903aa928dd89a0cd39634f88649f885abc3104
Viet <vtn1989@gmail.com>
2016-03-28 21:09:30 -0400

    found apis for stats, so no more need for x-ray module, added factories for getting player names, and game logs

--- app.js
+++ app.js
@@ -1,15 +1,83 @@
 'use strict';
 var app = angular.module('watchList', []);
-app.controller('MainCtrl', ['$scope', function($scope){
+app.config(['$httpProvider', function($httpProvider) {
+        $httpProvider.defaults.useXDomain = true;
+        delete $httpProvider.defaults.headers.common['X-Requested-With'];
+    }
+]);
+
+app.controller('MainCtrl', ['$scope', 'players', 'playerGameLog', function($scope, players, playerGameLog){
 	$scope.names = ["john", "bill", "charlie", "robert", "alban", "oscar", "marie", "celine", "brad", "drew", "rebecca", "michel", "francis", "jean", "paul", "pierre", "nicolas", "alfred", "gerard", "louis", "albert", "edouard", "benoit", "guillaume", "nicolas", "joseph"];
 	$scope.watchList = [];
 	$scope.Add = function(name){
 		$scope.watchList.push(name);
 	}
+	
+	// players.query().then(function(data){
+	// 	console.log(data);
+	// }, function(err){
+	// 	console.log('no data');
+	// });
+
+	// playerGameLog.query().then(function(data){
+	// 	console.log(data);	
+	// }, function(err){
+	// 	console.log(err);
+	// });
 
 }]);
-//use service to grab data
-//app.factory('PlayerData', [function(){}]);
+
+//use service to grab list of active players
+//http://stackoverflow.com/questions/24134117/no-access-control-allow-origin-header-is-present-on-the-requested-resource-an
+//^^^ above has some answers for when the request cannot process because the code is in a local env and its trying to request from a remote server or something
+//TODO: make grab players for current season
+app.factory('players',['$http', function($http){
+	var url = 'http://stats.nba.com/stats/commonallplayers?IsOnlyCurrentSeason=1&LeagueID=00&Season=2015-16';
+	var factory = {
+		query: function () {
+			var config = {
+				url: url,
+				method: 'GET'
+			};
+			var data = $http(config).then(function(result){
+				return result;
+			},
+			function(result){
+				console.log('error: no data');
+			});
+			return data;
+		}
+	}
+	return factory;
+}]);
+
+//service for grab a specified players game logs
+//TODO: place player name into this function and spit out player data
+app.factory('playerGameLog', ['$http', function($http){
+	var factory = {
+		query: function () {
+			var playerName = 'goran-dragic';
+			var config = {
+				url: 'https://www.stattleship.com/basketball/nba/game_logs?player_id=nba-' + playerName,
+				method: 'GET',
+				headers: {
+					'Content-Type': 'application/json',
+					'Authorization': 'Token token=371939afa66fa11f4d1095251c7c9f65',
+					'Accept': 'application/vnd.stattleship.com; version=1'
+				}
+			};
+			var data = $http(config).then(function(result){
+				return result;
+			},
+			function(result){
+				console.log('error: no data');
+			});
+			return data;
+		}
+	}
+	return factory;
+}]);
+
 app.directive('autoComplete', ['$timeout', function($timeout){
     return {
 			restrict: 'A',
--- server.js
+++ server.js
@@ -1,19 +1,35 @@
 'use strict';
 var express = require('express');
 var app = express();
-var xray = require('x-ray')();
+// var xray = require('x-ray')();
+// var http = require('http')();
 app.use(express.static(__dirname + '/'));
 app.listen(process.env.PORT || 8000);
 console.log('Magic happens on port 8000');
 
-xray('http://www.rotoworld.com/log/nba/1967/anthony-davis', {
-	playerName: 'h1',
-	stats: xray('.statstable', ['.statstable:not(#cp1_ctl02_tblDepthCharts) tr td'])})(function(err, obj){
+// var arr = new Array();
+// xray('http://www.rotoworld.com/log/nba/1967/anthony-davis', {
+// 	playerName: 'h1',
+// 	stats: xray('.statstable', ['.statstable:not(#cp1_ctl02_tblDepthCharts) tr td'])})(function(err, obj){
+// 		var size = 22;
+// 		for (var i=0; i<obj.stats.length; i+=size) {
+// 			arr.push(obj.stats.slice(i,i+size));
+// 		}
 
-  console.log(obj.stats);
-});
-//big array reduce
-// array.map(function(val, index, arr){})
-// inside: push to inner array while index +1 is != 0 for i%22
+// });
+
+// app.get('/data', function(req, res){
+// 	res.send(arr);
+// });
+
+// app.get('/players', function(req, res){
+// 	var options={
+// 		host: 'https://www.stattleship.com',
+// 		path: '/basketball/nba/players'
+// 	};
+// 	http.get(options, function(res){
+
+// 	})
+// });
 
 exports = module.exports = app;
